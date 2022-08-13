c872c917e0bda81cf9a66cc15ddd47b754fb91f9
BHAVSARHINAL <110092239+BHAVSARHINAL@users.noreply.github.com>
2022-08-13 14:04:21 +0530

    added

---
+++ escapesequence.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	
+	
+	
+	printf( "enter any number :\n");
+	scanf( "%d",&num);
+	
+	
+	   int a=4,b=8;
+	printf("\n%d" ,a+b);
+
+	
+	
+	
+	
+	
+	
+	return 0;
+}
---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	
+	int a=5,b=6;
+	printf("%d" ,a+b);
+
+	
+	
+	return 0;
+}
